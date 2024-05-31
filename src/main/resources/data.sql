fb183e6ec53087ba57c4bffea987e5bc7108c555
394-student013 <394-student013@la-study.com>
2024-05-31 11:00:10 +0900

    こみっと

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO account(id, name, grade, department, email, address, password) VALUES(1, '片山', 1, '〇学部', 'aa@aaa', 'ドーミーイン', '111');
+
+INSERT INTO item(id, name, lecture, professor, price) VALUES(1, '〇〇学入門', '〇〇学', '植竹', 1000);
---
+++ src/main/resources/textbook_db.sql
@@ -0,0 +1,45 @@
+-- 各種テーブル削除
+DROP TABLE IF EXISTS account;
+DROP TABLE IF EXISTS item;
+DROP TABLE IF EXISTS depertment;
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS order_details;
+
+-- 会員テーブル
+CREATE TABLE account
+(
+   id SERIAL PRIMARY KEY,
+   name TEXT,
+   grade INTEGER,
+   department TEXT,
+   email TEXT,
+   address TEXT,
+   password TEXT
+);
+-- 教科書テーブル
+CREATE TABLE item
+(
+   id SERIAL PRIMARY KEY,
+   name TEXT,
+   lecture TEXT,
+   professor TEXT,
+   price INTEGER
+);
+
+-- 注文テーブル
+CREATE TABLE orders
+(
+   id SERIAL PRIMARY KEY,
+   member_id INTEGER,
+   ordered_on DATE,
+   total_price INTEGER
+   
+);
+-- 注文明細テーブル
+CREATE TABLE order_details
+(
+   id SERIAL PRIMARY KEY,
+   order_id INTEGER,
+   textbook_id INTEGER,
+   quantity INTEGER
+);
