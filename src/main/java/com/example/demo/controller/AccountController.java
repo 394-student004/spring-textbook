204ff7f5107b8b53670997415a749fab61bf9257
394-student013 <394-student013@la-study.com>
2024-05-31 15:26:38 +0900

    まだエラーまみれだよ

--- src/main/java/com/example/demo/controller/AccountController.java
+++ src/main/java/com/example/demo/controller/AccountController.java
@@ -1,13 +1,27 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.demo.entity.Item;
+import com.example.demo.model.Account;
+
 @Controller
 public class AccountController {
+
+	@Autowired
+	Account account;
+
+	@Autowired
+	AccountRepository customerRepository;
 
 	// ログイン画面表示
 	@GetMapping({ "/", "/logout" })
@@ -21,20 +35,118 @@ public class AccountController {
 			@RequestParam(name = "email", defaultValue = "") String email,
 			@RequestParam(name = "password", defaultValue = "") String password,
 			Model model) {
+		// 名前が空の場合にエラーとする
+		if (email.length() == 0 || password.length() == 0) {
+			model.addAttribute("message", "入力してください");
+			return "login";
+		}
+		List<Account> customerList = accountRepository.findByEmailAndPassword(email, password);
+		if (customerList == null || customerList.size() == 0) {
+			// 存在しなかった場合
+			model.addAttribute("message", "メールアドレスとパスワードが一致しませんでした");
+			return "login";
+		}
+		Account account = accountList.get(0);
+
+		// セッション管理されたアカウント情報にIDと名前をセット
+		account.setId();
+		account.setName();
+
+		// 「/function」機能一覧画面へのリダイレクト
 		return "redirect:/function";
 	}
 
 	// 新規会員登録画面表示
+	@GetMapping("/account")
+	public String create() {
+		return "accountForm";
+	}
 
 	// 新規会員登録情報入力
+	@PostMapping("/account")
+	public String store(
+			@RequestParam(name = "name", defaultValue = "") String name,
+			@RequestParam(name = "grade", defaultValue = "") Integer grade,
+			@RequestParam(name = "department", defaultValue = "") String department,
+			@RequestParam(name = "email", defaultValue = "") String email,
+			@RequestParam(name = "address", defaultValue = "") String address,
+			@RequestParam(name = "password", defaultValue = "") String password,
+			Model model) {
 
+		// エラーチェック
+		List<String> errorList = new ArrayList<>();
+		if (name.length() == 0) {
+			errorList.add("名前は必須です");
+		}
+		if (grade == null) {
+			errorList.add("学年は必須です");
+		}
+		if (department.length() == 0) {
+			errorList.add("学部は必須です");
+		}
+		if (email.length() == 0) {
+			errorList.add("メールアドレスは必須です");
+		}
+		if (address.length() == 0) {
+			errorList.add("住所は必須です");
+		}
+		if (password.length() == 0) {
+			errorList.add("パスワードは必須です");
+		}
+
+		// メールアドレス存在チェック
+		List<Customer> customerList = customerRepository.findByEmail(email);
+		if (customerList != null && customerList.size() > 0) {
+			// 登録済みのメールアドレスが存在した場合
+			errorList.add("登録済みのメールアドレスです");
+		}
+		if (password.length() == 0) {
+			errorList.add("パスワードは必須です");
+		}
+
+		// エラー発生時はお問い合わせフォームに戻す
+		if (errorList.size() > 0) {
+			model.addAttribute("errorList", errorList);
+			model.addAttribute("name", name);
+			model.addAttribute("grade", grade);
+			model.addAttribute("department", department);
+			model.addAttribute("email", email);
+			model.addAttribute("address", address);
+			model.addAttribute("password", password);
+			return "accountForm";
+		}
+		Customer customer = new Customer(name, address, tel, email, password);
+		customerRepository.save(customer);
+
+		return "";
+	}
 	// 新規会員登録確認画面表示
 
 	// 新規登録処理
+	//	return "redirect:/";
 
 	// 会員情報変更画面表示
+	@GetMapping("/items/{id}/edit")
+	public String edit(@PathVariable("id") Integer id, Model model) {
+
+		Item item = itemRepository.findById(id).get();
+		model.addAttribute("item", item);
+		return "editItem";
+	}
 
 	// 会員情報変更内容入力
+	@PostMapping("/items/{id}/edit")
+	public String update(
+			@PathVariable("id") Integer id,
+			@RequestParam(value = "categoryId", defaultValue = "") Integer categoryId,
+			@RequestParam(value = "name", defaultValue = "") String name,
+			@RequestParam(value = "price", defaultValue = "") Integer price,
+			Model model) {
+
+		Item item = new Item(id, categoryId, name, price);
+		itemRepository.save(item);
+		return "redirect:/items";
+	}
 
 	// 会員情報変更内容確認画面表示
 
--- src/main/java/com/example/demo/controller/CartController.java
+++ src/main/java/com/example/demo/controller/CartController.java
@@ -1,14 +1,23 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entity.Item;
+import com.example.demo.model.Cart;
+import com.example.demo.repository.ItemRepository;
 
 @Controller
 public class CartController {
+
+	@Autowired
+	Cart cart;
+
+	@Autowired
+	ItemRepository itemRepository;
 
 	// カートの内容を表示
 	@GetMapping("/cart")
