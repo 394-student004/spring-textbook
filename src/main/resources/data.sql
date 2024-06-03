8415fd9a3658464cdc206d50a1d368df5b855bb8
394-student004 <394-student004@la-study.com>
2024-06-03 17:33:44 +0900

    有能人材になろう

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,10 +2,10 @@ INSERT INTO account(name, grade, department, email, address, password) VALUES('�
 INSERT INTO account(name, grade, department, email, address, password) VALUES('川村ハルカ', 2, '行動心理学部', 'bb@bbb', 'センチュリーパークタワー', '222');
 
 INSERT INTO item(name, lecture, professor, price) VALUES('よくわかる心理学', '心理学', '植竹セイヤ', 7000);
-INSERT INTO item(name, lecture, professor, price) VALUES('有能人材学入門~心が変われば行動が変わる~講演DVD付き特装版', '有能人材学', '茶谷アスカ', 10000);
+INSERT INTO item(name, lecture, professor, price) VALUES('有能人材入門~心が変われば行動が変わる~講演DVD付き特装版', '有能人材学', '茶谷アスカ', 10000);
 INSERT INTO item(name, lecture, professor, price) VALUES('有能人材基礎~行動が変われば習慣が変わる~講演DVD付き特装版', '有能人材学', '茶谷アスカ', 14000);
-INSERT INTO item(name, lecture, professor, price) VALUES('有能人材学発展~習慣が変われば人格が変わる~講演DVD付き特装版', '有能人材学', '茶谷アスカ', 20000);
-INSERT INTO item(name, lecture, professor, price) VALUES('有能人材学応用~人格が変われば運命が変わる~講演DVD付き特装版', '有能人材学', '茶谷アスカ', 1000);
+INSERT INTO item(name, lecture, professor, price) VALUES('有能人材発展~習慣が変われば人格が変わる~講演DVD付き特装版', '有能人材学', '茶谷アスカ', 20000);
+INSERT INTO item(name, lecture, professor, price) VALUES('有能人材応用~人格が変われば運命が変わる~講演DVD付き特装版', '有能人材学', '茶谷アスカ', 1000);
 INSERT INTO item(name, lecture, professor, price) VALUES('レギュレーションＦ完全対策', '歴史学', '片山ヒロキ', 3000);
 INSERT INTO item(name, lecture, professor, price) VALUES('インターネット用語集', '言語学', '植竹セイヤ', 2500);
 INSERT INTO item(name, lecture, professor, price) VALUES('方言研究学2005年改訂版', '言語学', '植竹セイヤ', 3300);
