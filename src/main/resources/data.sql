49b9501af8948cf5bc362f30dcf6207133edc42a
394-student004 <394-student004@la-study.com>
2024-06-03 16:01:31 +0900

    データの一部修正

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
 INSERT INTO account(name, grade, department, email, address, password) VALUES('片山ヒロキ', 1, '人間科学学部', 'aa@aaa', 'ドーミーイン', '111');
 INSERT INTO account(name, grade, department, email, address, password) VALUES('川村ハルカ', 2, '行動心理学部', 'bb@bbb', 'センチュリーパークタワー', '222');
 
-INSERT INTO item(name, lecture, professor, price) VALUES('〇〇学入門', '心理学', '植竹セイヤ', 7000);
+INSERT INTO item(name, lecture, professor, price) VALUES('心理学入門', '心理学', '植竹セイヤ', 7000);
 INSERT INTO item(name, lecture, professor, price) VALUES('有能人材学入門~心が変われば行動が変わる~講演DVD付き特装版', '有能人材学', '茶谷アスカ', 10000);
 INSERT INTO item(name, lecture, professor, price) VALUES('有能人材学発展~行動が変われば習慣が変わる~講演DVD付き特装版', '有能人材学', '茶谷アスカ', 15000);
 INSERT INTO item(name, lecture, professor, price) VALUES('有能人材学応用~習慣が変われば人格が変わる~講演DVD付き特装版', '有能人材学', '茶谷アスカ', 20000);
-INSERT INTO item(name, lecture, professor, price) VALUES('レギュレーションＦ完全対策', '歴史学', '鎌田ゴウキ', 3000);
+INSERT INTO item(name, lecture, professor, price) VALUES('レギュレーションＦ完全対策', '歴史学', '真多後ウキカ', 3000);
 
